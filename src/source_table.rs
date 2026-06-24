89469684892576b22277186a048ae5cc7644dd9c
xDestructo <sarthakmohit27@gmail.com>
2026-06-25 04:23:11 +0530

    Added type resolution functionality to Symbol Table
    Upgraded the symbol table to track aliases like typedef and #define macros alongside variables. As so many people use #define ll long long etc.
    
    - Replaced the raw hashmap with a scope struct
    - added aliases map
    - implemented recursive unwrapping of aliased types

--- src/source_table.rs
+++ src/source_table.rs
@@ -13,20 +13,26 @@ pub struct VariableState {
     pub line_declared: usize,  
 }
 
+#[derive(Debug, Default)]
+pub struct Scope {
+    pub variables: HashMap<String, VariableState>,
+    pub aliases: HashMap<String, String>, // Tracks things like ll -> long long
+}
+
 #[derive(Debug)]
 pub struct SymbolTable {
-    scopes: Vec<HashMap<String, VariableState>>,
+    scopes: Vec<Scope>,
 }
 
 impl SymbolTable {
     pub fn new() -> Self {
         Self {
-            scopes: vec![HashMap::new()], 
+            scopes: vec![Scope::default()], 
         }
     }
 
     pub fn enter_scope(&mut self) {
-        self.scopes.push(HashMap::new());
+        self.scopes.push(Scope::default());
     }
 
     pub fn exit_scope(&mut self) {
@@ -34,19 +40,36 @@ impl SymbolTable {
             self.scopes.pop();
         }
     }
-    pub fn declare(&mut self, name: String, state: VariableState) {
+
+    pub fn declare_var(&mut self, name: String, state: VariableState) {
         if let Some(current_scope) = self.scopes.last_mut() {
-            current_scope.insert(name, state);
+            current_scope.variables.insert(name, state);
         }
     }
 
-    pub fn lookup_mut(&mut self, name: &str) -> Option<&mut VariableState> {
+    pub fn declare_alias(&mut self, alias: String, actual_type: String) {
+        if let Some(current_scope) = self.scopes.last_mut() {
+            current_scope.aliases.insert(alias, actual_type);
+        }
+    }
+
+    pub fn lookup_var(&mut self, name: &str) -> Option<&mut VariableState> {
         for scope in self.scopes.iter_mut().rev() {
-            if let Some(state) = scope.get_mut(name) {
+            if let Some(state) = scope.variables.get_mut(name) {
                 return Some(state);
             }
         }
         None // undeclared var
     }
 
+    pub fn resolve_type(&self, type_name: &str) -> String {
+        let mut current_type = type_name.to_string();
+        
+        for scope in self.scopes.iter().rev() {
+            if let Some(actual_type) = scope.aliases.get(&current_type) {
+                current_type = actual_type.clone();
+            }
+        }
+        current_type
+    }
 }
